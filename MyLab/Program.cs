a34e1a050140a41c657464dd988e08836a8bf855
Hakim Ghods <ghods.hakim@hotmail.com>
2019-07-16 18:07:52 +0430

    fix

--- MyLab/Program.cs
+++ MyLab/Program.cs
@@ -8,5 +8,47 @@ namespace MyLab
         {
             Console.WriteLine("Hello World!");
         }
+
+        public abstract class Shape {
+            public int Color { get; set; }
+            public abstract float Area();
+            //public virtual float Area() {
+
+            //    return 0;
+            //}
+        }
+
+        public class Circle : Shape
+        {
+            public int Radius { get; set; }
+            public override float Area()
+            {
+                return Radius* Radius*3.14f;
+            }
+        }
+
+        public class Ellipse : Circle
+        {
+            public override float Area()
+            {
+                return base.Area();
+            }
+        }
+
+        public class Line : Shape
+        {
+            public override float Area()
+            {
+                // line has no area
+                throw new InvalidOperationException();
+            }
+        }
+
+        public interface IArea
+        {
+
+        }
+
+
     }
 }
