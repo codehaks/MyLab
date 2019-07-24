8416d29083b5fdba62acd4917b0fefd501cc7251
Hakim Ghods <ghods.hakim@hotmail.com>
2019-07-24 18:34:18 +0430

    Parallel

--- MyLab/Program.cs
+++ MyLab/Program.cs
@@ -7,9 +7,54 @@ namespace MyLab
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var filePath = @"D:\Projects\Data\books\Pride_and_prejudice.txt";
+
+            var textAnalysis = new TextAnalysis();
+            var count=textAnalysis.FindWord("Jane", filePath);
+
+            Console.WriteLine($" Count : {count}");
+            
         }
 
+    }
 
+    class TextAnalysis
+    {
+        public int FindWord(string word,string filePath)
+        {
+            var text = System.IO.File.ReadAllText(filePath);
+            var wordList = text.Split(' ');
 
+            int count = 0;
+
+            for (int i = 0; i < wordList.Length; i++)
+            {
+                if (word==wordList[i])
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int FindWordParallel(string word, string filePath)
+        {
+            var text = System.IO.File.ReadAllText(filePath);
+            var wordList = text.Split(' ');
+
+            int count = 0;
+
+            for (int i = 0; i < wordList.Length; i++)
+            {
+                if (word == wordList[i])
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
     }
 }
