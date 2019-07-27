7ec55c4643032a4cc4b90aeb940d384004d0daea
Hakim Ghods <ghods.hakim@hotmail.com>
2019-07-28 01:34:20 +0430

    Delegates

--- MyLab/Program.cs
+++ MyLab/Program.cs
@@ -4,12 +4,26 @@ namespace MyLab
 {
     class Program
     {
+        public delegate void Notify(string message);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Notify notifier = (m) => { Console.WriteLine(m); };
+            notifier += (m) => { Console.WriteLine(" Message : " + m); };
+
+            WorkStarted(notifier);
+            WorkFinished(notifier);
+
         }
 
+        static public void WorkStarted(Notify notify)
+        {
+            notify("started");
+        }
 
-
+        static public void WorkFinished(Notify notify)
+        {
+            notify("Finished");
+        }
     }
 }
