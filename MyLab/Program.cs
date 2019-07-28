c0826b794b8658319b2c8692faf077f80b39a773
Hakim Ghods <ghods.hakim@hotmail.com>
2019-07-28 16:32:11 +0430

    Race

--- MyLab/Program.cs
+++ MyLab/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyLab
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int[] numbers = Enumerable.Range(0, 1000_000).ToArray();
+
+        public static int count = 0;
+
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var t1=Task.Factory.StartNew(IsOdd);
+            var t2=Task.Factory.StartNew(IsEven);
+
+            await Task.WhenAll(t1, t2);
+
+            Console.WriteLine($" Count : {count}");
+
+
         }
 
+        public static void IsOdd()
+        {
+            foreach (var number in numbers)
+            {
+                if (number % 2 !=0)
+                {
+                    count--;
+                }
+            }
+        }
 
-
+        public static void IsEven()
+        {
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    count++;
+                }
+            }
+        }
     }
+
+    
 }
