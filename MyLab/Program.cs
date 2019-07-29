9b802c2c479021e66b9e212ee754da7d8e360b2f
Hakim Ghods <ghods.hakim@hotmail.com>
2019-07-30 00:15:15 +0430

    Parallel

--- MyLab/Program.cs
+++ MyLab/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MyLab
 {
@@ -7,6 +9,24 @@ namespace MyLab
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Parallel.For(0, 10, (index) => ShowTime(index));
+
+            Parallel.ForEach("Hello World".ToCharArray(), (c) => ShowChar(c));
+            
+        }
+
+        public static void ShowTime(int threadId)
+        {
+            Thread.Sleep(new Random().Next(100,500));
+            Console.WriteLine($"{threadId} -> {DateTime.Now.Second}:{DateTime.Now.Millisecond}");
+        }
+
+
+        public static void ShowChar(char c)
+        {
+            Thread.Sleep(new Random().Next(100, 500));
+            Console.WriteLine($"{c} -> {DateTime.Now.Second}:{DateTime.Now.Millisecond}");
         }
 
 
