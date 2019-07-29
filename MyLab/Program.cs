2c31cc96ed3aae825c821b0df0434617958e3b76
Hakim Ghods <ghods.hakim@hotmail.com>
2019-07-30 00:27:42 +0430

    Options

--- MyLab/Program.cs
+++ MyLab/Program.cs
@@ -10,15 +10,20 @@ namespace MyLab
         {
             Console.WriteLine("Hello World!");
 
-            Parallel.For(0, 10, (index) => ShowTime(index));
+            //ParallelOptions options
+            //= new ParallelOptions() { MaxDegreeOfParallelism = 4 };
+            //Parallel.For(0, 10,options, (index) => ShowTime(index));
 
+
+            Parallel.For(0, 10, (index) => ShowTime(index));
             Parallel.ForEach("Hello World".ToCharArray(), (c) => ShowChar(c));
-            
+
         }
 
         public static void ShowTime(int threadId)
         {
-            Thread.Sleep(new Random().Next(100,500));
+            //Thread.Sleep(new Random().Next(100,500));
+            //Thread.Sleep(1000);
             Console.WriteLine($"{threadId} -> {DateTime.Now.Second}:{DateTime.Now.Millisecond}");
         }
 
